3e4b84f84657773e227485eb65fa864bc29aa202
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-05 21:41:37 +0000

    Added a route for /urls and created it's ejs template that consists of a table (with both the long and short URL) and a link to create a new URL

--- express_server.js
+++ express_server.js
@@ -18,10 +18,17 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+
 
 
 app.listen(PORT, () => {
