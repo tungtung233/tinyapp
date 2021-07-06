d9773cbbcbf89246e524a662e7ab0163595cc9c0
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-06 04:40:37 +0000

    Added a delete button to our /urls_index page - it deletes the corresponding shortURL from our database and updates the table

--- express_server.js
+++ express_server.js
@@ -45,6 +45,12 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL]
+  res.redirect(`/urls`)
+});
+
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
