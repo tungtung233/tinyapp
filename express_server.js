d1e3070c6150ef0cb2258bf5806fa2acc476820f
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-06 02:30:19 +0000

    Added body-parser and a generateRandomString function

--- express_server.js
+++ express_server.js
@@ -4,6 +4,10 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs") 
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -14,14 +18,19 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 
@@ -32,11 +41,41 @@ app.get("/urls/:shortURL", (req, res) => {
 
 
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+
+function generateRandomString() {
+  let randomString = "";
+
+  /* 0-9 = ASCII codes 48-57
+    A-Z = ASCII codes 65-90
+    a-z = ASCII codes 97-122 */
+
+  // generates a random number between 48-122, inclusive of min and max, and then converts that into it's corresponding ASCII character.
+  // but there are other random punctuations included in this range, including the underscore _
+  const randomAlphanumericCharacter = function () {
+    return String.fromCharCode(Math.random() * (122 - 48 + 1) + 48);
+  }
+
+  while (randomString.length < 6) {
+    const character = randomAlphanumericCharacter();
+
+    if (/[0-9A-Za-z]/.test(character)) {
+      randomString += character;
+    }
+  }
+  
+  return randomString
+
+}
 
 
 app.listen(PORT, () => {
