3125f30cf2618bd28c487823e53be804e6212895
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-06 03:39:40 +0000

    When a user inputs a long URL, they will get redirected to /urls/:shortURL (browser renders /urls_show), where they can see their new shortURL and click on a link in there that redirects them to their original long URL, through our new shortURL

--- express_server.js
+++ express_server.js
@@ -24,8 +24,9 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`)
 });
 
 
@@ -35,10 +36,19 @@ app.get("/urls/new", (req, res) => {
 
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  if (urlDatabase[req.params.shortURL]) {
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
+  } else {
+    res.send('ERROR: 404 Page Not Found')
+  }
 });
 
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
 
 
 
